7706dc9dad56eefc260592508b1ec97b11b263c2
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2022-12-15 22:16:29 -0500

    login refactor and error handling with errorDialogs, as well as verification email pop up

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/views/login_view.dart';
 
 class VerifiyEmailView extends StatefulWidget {
   const VerifiyEmailView({super.key});
@@ -18,15 +17,49 @@ class _VerifiyEmailViewState extends State<VerifiyEmailView> {
         children: [
           const Text('Send Email Verification'),
           TextButton(
-            style: raisedButtonStyle,
+            // style: raisedButtonStyle,
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
+              await user?.sendEmailVerification().then(
+                (value) async {
+                  await emailVerificationDialog(
+                    context,
+                    user.email,
+                  );
+                },
+              );
             },
             child: const Text('Send Email Verification'),
-          )
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: const Text("Sign Out"),
+          ),
         ],
       ),
     );
   }
+}
+
+Future<void> emailVerificationDialog(BuildContext context, email) {
+  return showDialog(
+    context: context,
+    builder: ((context) {
+      return AlertDialog(
+        title: const Text("Verification Email Sent"),
+        content: Text(
+            "Please check your spam folder in your email for the following email: $email"),
+        actions: [
+          TextButton(
+            onPressed: (() {
+              Navigator.of(context).pop();
+            }),
+            child: const Text("Ok"),
+          )
+        ],
+      );
+    }),
+  );
 }
